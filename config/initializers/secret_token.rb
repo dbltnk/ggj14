8fa03a9cbae8a2039fb12d4d8a99387d30d37c0e
Alexander Zacherl <alexander.zacherl@googlemail.com>
2014-01-24 21:56:38 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ggj14::Application.config.secret_key_base = '841e3b24ce7a4f713b3b77f12ec5a0e2015d9e15b41aa849fce3dacd82f66c1f821c63fe0ca332dadcf5ac699addc3a8a7f5d71ac1e4014fedb134dda22a0cbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ggj14::Application.config.session_store :cookie_store, key: '_ggj14_session'
